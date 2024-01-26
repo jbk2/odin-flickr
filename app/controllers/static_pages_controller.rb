9c47963cf0e681cc01786ad55b5c41f18c91a0bd
jbk2 <james@humanbenefits.com>
2024-01-26 17:23:39 +0100

    Refactor controller action logic

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,17 +1,17 @@
 class StaticPagesController < ApplicationController
   def home
-
-  end
-
-  def flickr_users_images
-    @flickr_user_id = params[:flickr_user_id]
-    flickr_service = FlickrService.new(@flickr_user_id)
-    @photos = flickr_service.fetch_photos
-
-    render 'home'
+    if params[:flickr_user_id]
+      @flickr_user_id = params[:flickr_user_id]
+      @photos = fetch_photos_for(@flickr_user_id) # Implement this method to fetch photos
+    end
   end
 
   private
+  def fetch_photos_for(flickr_user_id)
+    flickr_service = FlickrService.new(flickr_user_id)
+    @photos = flickr_service.fetch_photos
+  end
+
   def static_page_params
     params.require(:static_page).permit(:flickr_user_id)
   end
