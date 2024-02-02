faf3c58e4d4df2cd435f502f1ceb7139c654330e
jbk2 <james@humanbenefits.com>
2024-02-02 09:25:11 +0100

    Dorrect Daisyui config duplication

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'), require("daisyui"),
+    require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
     require("daisyui"),
   ],
